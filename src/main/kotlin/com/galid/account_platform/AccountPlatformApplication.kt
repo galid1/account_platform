0dafd86cf6083a7a7260741e678cbd7ae90cbcb7
전준엽 <galid1@naver.com>
2022-01-20 16:24:25 +0900

    refactor: application 확장자 yml로

---
+++ src/main/kotlin/com/galid/account_platform/AccountPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.galid.account_platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountPlatformApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccountPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/galid/account_platform/AccountPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.galid.account_platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountPlatformApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
