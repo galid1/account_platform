42388223421e190688c455cb14ef4d97be9a4ffd
전준엽 <galid1@naver.com>
2022-01-21 22:54:05 +0900

    feat: configure mustache

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,20 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-mustache
+    implementation("org.springframework.boot:spring-boot-starter-mustache:_")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // data
+    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ src/main/kotlin/com/galid/account_platform/presentation/controller/TestController.kt
@@ -0,0 +1,13 @@
+package com.galid.account_platform.presentation.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class TestController {
+    @GetMapping("/")
+    fun test(model: Model): String {
+        return "index"
+    }
+}
